20b765e6e835bd673f81985d554676c52b76f272
sniperhuni <jhoon49.kim@gmail.com>
2021-02-04 13:12:24 -0500

    add shortest path algo

--- MIT6002_graph_shortest_pass_DFS.py
+++ MIT6002_graph_shortest_pass_DFS.py
@@ -90,6 +90,74 @@ def testSP(source, destination):
     else:
         print('There is not path from ', source, 'to ', destination)
 
+'''
+Breadth first search
+Search from path with length=1 to the longest path
+
+We do not need shortest parameter in DFS
+'''
+def BFS(graph, start, end, toPrint=False):
+
+    '''
+    input: Digraph object
+
+    return: a shortest path from start to end in graph
+    '''
+
+    '''start with a start node'''
+    initPath = [start]
+    # This queue save lists of path
+    # e.g. [[A], [A,B], [A,C], [A,B,D] ...]
+    pathQueue = [initPath]
+
+    while len(pathQueue) !=0:
+
+        # Get and remove the oldest element in pathQueue
+        if toPrint:
+            print('Queue:', len(pathQueue))
+            for p in pathQueue:
+                print(printPath(p))
+
+        currentPath = pathQueue.pop(0)
+
+        if toPrint:
+            print('Current BFS path: ', printPath(currentPath))
+            print()
+
+        # the last node of the current path
+        lastNode = currentPath[-1]
+
+        # We found the solution
+        if lastNode == end:
+            return currentPath
+
+        '''
+        Before the current node is replaced by the new value,
+        add next candidate paths
+        '''
+        for nextNode in graph.childrenOf(lastNode):
+            if nextNode not in currentPath:
+                newPath = currentPath + [nextNode]
+                pathQueue.append(newPath)
+
+    return None
+
+def shortestPath_BFS(graph, start, end, toPrint = False):
+    return BFS(graph, start, end, toPrint)
+
+def testSP_BFS(source, destination):
+
+    g = buildCityGraph(Digraph)
+
+    sp = shortestPath_BFS(g, g.getNode(source), g.getNode(destination),
+                    toPrint = True)
+
+    if sp != None:
+
+        print('Shortest path from ',source, 'to ', destination, 'is ',
+                printPath(sp))
+    else:
+        print('There is not path from ', source, 'to ', destination)
 
 g = buildCityGraph(Digraph)
 print(g)
@@ -97,5 +165,9 @@ print('--------------------')
 
 # No path
 #testSP('Chicago', 'Boston')
-
+print('A solution of DFS')
 testSP('Boston', 'Phoenix')
+print('--------------------')
+
+print('A solution of BFS')
+testSP_BFS('Boston', 'Phoenix')
